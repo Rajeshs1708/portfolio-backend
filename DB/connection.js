d927ae83a15d7aab604da8d1a38b4b5cbbbd4033
Rajesh <rajesh@cstream.io>
2024-07-13 13:50:18 +0530

    new changes

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 exports.db = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .then(() => {
       console.log("Connection established");
     })
